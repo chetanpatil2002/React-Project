45a5455dff2ea0cb0587aee9f68ae913a25ce9b8
chetanpatil2002 <139349451+chetanpatil2002@users.noreply.github.com>
2024-06-07 05:37:04 -0400

    Add files via upload

---
+++ ReactStyling/Styling_Advance.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ShoppingHomepage from "./ShoppingHomepage";
+import "./Style.css";
+
+export default function Styling_Advance() {
+  return (
+    <div>
+      <ShoppingHomepage />
+    </div>
+  );
+}
